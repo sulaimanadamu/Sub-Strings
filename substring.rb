17feba06bf7909cd14febe3310b8d5c70a13cb05
sulaiman adamu <adamusulaiman2021@gmail.com>
2025-08-15 07:33:46 +0100

    created the substring

---
+++ substring.rb
@@ -0,0 +1,20 @@
+def substrings(word, normal_case_dictionary)
+    word_count = {}
+    word = word.downcase
+    dictionary = normal_case_dictionary.map(&:downcase)
+
+    dictionary.each do | selected_word | 
+        # are all of the character of the selected word member of the given word
+        subset = selected_word.chars.all? { |dic_word| word.chars.include?(dic_word) }
+        # create hash of count the number of occurrence of word in dictionary.
+        if subset
+            word_count[selected_word] = word.scan(selected_word).length
+        end
+    end
+    return word_count
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
