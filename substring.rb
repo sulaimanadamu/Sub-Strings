e64d7a3788b0c33a164ec6f2c590ac6c0f456936
sulaiman adamu <adamusulaiman2021@gmail.com>
2025-08-15 07:40:40 +0100

    Created a shorter solution using the scan method

--- substring.rb
+++ substring.rb
@@ -3,14 +3,21 @@ def substrings(word, normal_case_dictionary)
     word = word.downcase
     dictionary = normal_case_dictionary.map(&:downcase)
 
-    dictionary.each do | selected_word | 
-        # are all of the character of the selected word member of the given word
-        subset = selected_word.chars.all? { |dic_word| word.chars.include?(dic_word) }
-        # create hash of count the number of occurrence of word in dictionary.
-        if subset
-            word_count[selected_word] = word.scan(selected_word).length
+    dictionary.each do |selected_word| 
+        count = word.scan(selected_word).length
+        unless count == 0
+            word_count[selected_word] = count
         end
     end
+
+    # dictionary.each do | selected_word | 
+    #     # are all of the character of the selected word member of the given word
+    #     subset = selected_word.chars.all? { |dic_word| word.chars.include?(dic_word) }
+    #     # create hash of count the number of occurrence of word in dictionary.
+    #     if subset
+    #         word_count[selected_word] = word.scan(selected_word).length
+    #     end
+    # end
     return word_count
 
 end
